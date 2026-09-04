5937114e0bf5a6e579a9ad283189e5186c0a1804
DESKTOP-FM7R80P\user <heajin722@naver.com>
2026-09-04 15:33:54 +0900

    feat : 옵션 창에 읽기 speed 작업

---
+++ Assets/Scripts/UI/BibleReadView/Popup/UIOptionPopup.ReadingSpeed.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
+
+public partial class UIOptionPopup
+{
+    [UIInject("Slider_Speed")] private Slider Slider_Speed;
+    [UIInject("Image_Speed_Slider_Select_Bg")] private Image Image_Speed_Slider_Select_Bg;
+    private void OnSpeedChanged(float InSpeed)
+    {
+        var speed = Mathf.Round(InSpeed * 20F) / 20F;
+
+        BibleReadingSetting.Instance.TTSSpeed = speed;
+
+        Image_Speed_Slider_Select_Bg.fillAmount = speed * 0.5F;
+    }
+}
--- Assets/Scripts/UI/BibleReadView/Popup/UIOptionPopup.cs
+++ Assets/Scripts/UI/BibleReadView/Popup/UIOptionPopup.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using UnityEngine.Scripting;
 
-public class UIOptionPopup : UIPopupBase
+public partial class UIOptionPopup : UIPopupBase
 {
+    [UIInject("Button_Complete")] private UIButton Button_Complete;
+
     protected override void Start()
     {
-        base.Start();
+        UpdateContent();
         BindEvent();
+    }
+
+    public override void UpdateContent()
+    {
+        base.UpdateContent();
+
+        var speed = BibleReadingSetting.Instance.TTSSpeed;
+
+        Image_Speed_Slider_Select_Bg.fillAmount = speed * 0.5F;
+        Slider_Speed.value = speed;
     }
 
     protected override void OnDestroy()
@@ -13,4 +26,25 @@ public class UIOptionPopup : UIPopupBase
         UnBindEvent();
         base.OnDestroy();
     }
+
+    public override void BindEvent()
+    {
+        base.BindEvent();
+
+        UIBindEvent.BindEvent(Button_Complete, OnClickComplete);
+        Slider_Speed.onValueChanged.AddListener(OnSpeedChanged);
+    }
+
+    public override void UnBindEvent()
+    {
+        base.UnBindEvent();
+        UIBindEvent.UnBindEvent(Button_Complete, OnClickComplete);
+        Slider_Speed.onValueChanged.RemoveListener(OnSpeedChanged);
+    }
+
+    private void OnClickComplete()
+    {
+        base.OnClose();
+    }
 }
+
